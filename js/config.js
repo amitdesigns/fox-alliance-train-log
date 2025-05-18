317c3767bb88dd83bb1ccb0ab5c53cb4c133b6ea
MrDevilsEggs <85073884+amitdesigns@users.noreply.github.com>
2025-05-19 00:45:39 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,14 +5,6 @@ const firebaseConfig = {
   storageBucket: "fox-alliance-train-log.firebasestorage.app",
   messagingSenderId: "484011286679",
   appId: "1:484011286679:web:2e757e9e955ddf8cd349d2",
-  measurementId: "G-WNNG3GBJLZ"
+  
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore(app);
-const auth = firebase.auth(app);
-
-// Disable Firestore persistence (causes CSP issues)
-db.disablePersistence().catch((err) => {
-  console.log("Persistence disabled:", err);
-});
