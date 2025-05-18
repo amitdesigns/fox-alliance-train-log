94b764051298b20342a5fe76938397078b2bc92f
MrDevilsEggs <85073884+amitdesigns@users.noreply.github.com>
2025-05-19 00:25:57 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDesRiEQsIWQLoxcpzbss8kh7c9hH_LW64",
+  authDomain: "fox-alliance-train-log.firebaseapp.com",
+  projectId: "fox-alliance-train-log",
+  storageBucket: "fox-alliance-train-log.firebasestorage.app",
+  messagingSenderId: "484011286679",
+  appId: "1:484011286679:web:2e757e9e955ddf8cd349d2",
+  measurementId: "G-WNNG3GBJLZ"
+};
